050045e834487d33b16e18b9840dde0205ba2b9f
Abhay Koduru <k.abhaysanthosh@gmail.com>
2015-03-25 14:10:35 -0500

    Completed Response Object topic.

--- app.py
+++ app.py
@@ -12,12 +12,39 @@ app = Flask(__name__)
 
 # define the view using a function, which return a string
 def hello_world():
-	return "Hello, World!"
+	return "<b>Hello, World!</b>"
 
 @app.route("/home")
-
 def home():
 	return "Welcome home!"
+
+# dynamic route
+@app.route("/test/<search_query>")
+def search(search_query):
+	return search_query
+
+@app.route("/integer/<int:value>")
+def int_type(value):
+	print value + 1
+	return "correct"
+
+@app.route("/float/<float:value>")
+def float_type(value):
+	print value + 1
+	return "correct"
+
+@app.route("/path/<path:value>")
+def path_type(value):
+	print value
+	return "correct"
+
+@app.route("/name/<name>")
+def index(name):
+	if name.lower() == "michael":
+		return "Hello, {}".format(name)
+	else:
+		return "Not Found", 404
+		
 
 # start the development server using the run() method
 if __name__ == "__main__":
